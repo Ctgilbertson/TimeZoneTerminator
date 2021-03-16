55bc74dcecedc34c19cd0309d3f24e68d983fa3b
ctgilbertson <ctgilbertson@wisc.edu>
2021-03-16 11:54:54 -0500

    pickup

---
+++ Assets/Scripts/pickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+
+    public Transform destination;
+
+    void OnMouseDown()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+
+        this.transform.position = destination.position;
+
+        this.transform.parent = GameObject.Find("destination").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<SphereCollider>().enabled = true;
+
+
+    }
+
+}
+
