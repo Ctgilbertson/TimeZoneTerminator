077197b2def655c73f62bce22ff91be0ce5972be
ctgilbertson <ctgilbertson@wisc.edu>
2021-04-26 20:28:00 -0500

    updates

--- Assets/Scripts/Controller/MouseLook.cs
+++ Assets/Scripts/Controller/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 999999999f;
+    public float mouseSensitivity = 10f;
 
     public Transform playerBody;
     public Transform weapons;
@@ -32,8 +32,66 @@ public class MouseLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         //Vector3.up isolates the Y axis to move in relation to the mouseX
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+       // playerBody.rotation = Quaternion.Euler(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
     }
 }
+
+/*
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    private Rigidbody rb;
+    float rotationY = 0F;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+            rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+}
+*/
---
+++ Assets/Scripts/DeskLock.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskLock : MonoBehaviour
+{
+
+    public Transform destination;
+    public GameObject uiObject;
+    public GameObject camera;
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        float dist = Vector3.Distance(this.transform.position, destination.position);
+
+        if (dist < 5f)
+        {
+
+            camera.SetActive(true);
+            playerCamera.SetActive(false);
+
+        }
+    }
+
+
+    void OnMouseUp()
+    {
+        playerCamera.SetActive(true);
+        camera.SetActive(false);
+
+    }
+
+}
